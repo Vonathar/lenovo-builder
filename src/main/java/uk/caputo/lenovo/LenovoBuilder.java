849b638e8fe1e7ab9cc4ffd9526ccca8ca444a48
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-07 18:40:58 +0000

    Add main SpringBootApplication class.

---
+++ src/main/java/uk/caputo/lenovo/LenovoBuilder.java
@@ -0,0 +1,12 @@
+package uk.caputo.lenovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenovoBuilder {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LenovoBuilder.class, args);
+  }
+}
