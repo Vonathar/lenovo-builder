42bf4f197cf4ac9cffd94a2a7551a4664c4d27bc
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-08 12:09:46 +0000

    Update line wrap (80) in BlueprintReader class.

--- src/main/java/uk/caputo/lenovo/util/BlueprintReader.java
+++ src/main/java/uk/caputo/lenovo/util/BlueprintReader.java
@@ -2,6 +2,7 @@ package uk.caputo.lenovo.util;
 
 import java.io.File;
 import java.util.Scanner;
+import uk.caputo.lenovo.ComponentType;
 
 /**
  * A class used to read content stored in HTML files in the resources folder.
@@ -10,19 +11,22 @@ import java.util.Scanner;
  */
 public class BlueprintReader {
 
-  public BlueprintReader() {}
+  public BlueprintReader() {
+  }
 
   /**
-   * Reads and returns the content of a given HTML file. All files should be located in the /html
-   * directory in order to be found.
+   * Reads and returns the content of a given HTML file. All files should be
+   * located in the /html directory in order to be found.
    *
-   * @param fileName the name of the file to parse, without the .html extension.
+   * @param type the type of the file to parse.
    * @return the HTML blueprint as it is in the file.
    */
-  public String getContent(String fileName) {
+  public String getContent(ComponentType type) {
+    String fileName = type.toString().toLowerCase();
     StringBuilder blueprint = new StringBuilder();
     try {
-      File templateFile = new File(String.format("src/main/resources/html/%s.html", fileName));
+      File templateFile = new File(
+          String.format("src/main/resources/html/%s.html", fileName));
       Scanner scanner = new Scanner(templateFile);
       while (scanner.hasNextLine()) {
         blueprint.append(scanner.nextLine());
