2d2aee112a3d6306dea14fcf05347c3ecf156c29
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-08 13:34:33 +0000

    Add skeleton for main test class.

---
+++ src/test/java/uk/caputo/lenovo/LenovoBuilderTests.java
@@ -0,0 +1,11 @@
+package uk.caputo.lenovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenovoBuilderTests {
+
+  @Test
+  void contextLoads() {}
+}
