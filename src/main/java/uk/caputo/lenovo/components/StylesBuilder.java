3b01c9da1c7ae705fc433032b377b7b97a281cd0
Gianmarco Caputo <gianmarco.caputo@wearetwogether.com>
2020-11-18 15:13:42 +0000

    Implement buildBackgroundStyles in StylesBuilder.
    Since the API accepts both a backgrund image URL and a background colour, this will prevent them from both being added in the HTML template.

--- src/main/java/uk/caputo/lenovo/components/StylesBuilder.java
+++ src/main/java/uk/caputo/lenovo/components/StylesBuilder.java
@@ -15,12 +15,32 @@ public class StylesBuilder {
    * Replaces all placeholder tokens from the component's blueprint with values
    * from the Settings object.
    *
+   * @param markup   the HTML blueprint with tokens.
+   * @param settings the component settings.
    * @return The HTML markup with populated tokens.
    */
   public String replaceTokens(String markup, Settings settings) {
     return markup
         .replace("$wrapperClassName", settings.getWrapperClassName())
         .replace("$primaryFontSize", settings.getPrimaryFontSize())
-        .replace("$secondaryFontSize", settings.getSecondaryFontSize());
+        .replace("$secondaryFontSize", settings.getSecondaryFontSize())
+        .replace("$backgroundStyles", buildBackgroundStyles(settings));
+  }
+
+  /**
+   * Builds the CSS declaration for the component background. The background
+   * image is used if available, or the background colour as a fallback.
+   *
+   * @param settings the component settings.
+   * @return The CSS declaration for the background style.
+   */
+  private String buildBackgroundStyles(Settings settings) {
+    if (settings.getBackgroundImageUrl() != null) {
+      return String.format("background-image: url(\"%s\")",
+          settings.getBackgroundImageUrl());
+    } else {
+      return String
+          .format("background-color: %s", settings.getBackgroundColour());
+    }
   }
 }
